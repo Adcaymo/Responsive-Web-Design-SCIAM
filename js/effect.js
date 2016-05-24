91957941f73cd7e3344a3704186cfd1a9a17fb00
Adrian Caymo <adcaymo@gmail.com>
2016-05-23 21:05:07 -0700

    Add files via upload

---
+++ js/effect.js
@@ -0,0 +1,7 @@
+// Per our e-mail conversation, this is my own code.	
+
+$(document).ready(function () {
+	$("td").click(function() {
+		$(this).toggleClass("invert");
+	});	// end click
+});	// end ready
